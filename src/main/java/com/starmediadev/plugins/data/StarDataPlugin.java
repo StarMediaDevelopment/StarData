611dd663d1a60b88e325bc794fe97ef385acd70c
Firestar311 <slaterandrew0398@gmail.com>
2021-07-30 22:15:04 -0500

    Removed some testing stuff

--- src/main/java/com/starmediadev/plugins/data/StarDataPlugin.java
+++ src/main/java/com/starmediadev/plugins/data/StarDataPlugin.java
@@ -3,13 +3,7 @@ package com.starmediadev.plugins.data;
 import com.starmediadev.data.StarData;
 import com.starmediadev.data.manager.DatabaseManager;
 import com.starmediadev.data.manager.MultidatabaseManager;
-import com.starmediadev.data.properties.SqlProperties;
-import com.starmediadev.utils.Code;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Random;
 
 public class StarDataPlugin extends JavaPlugin {
     
@@ -20,52 +14,7 @@ public class StarDataPlugin extends JavaPlugin {
         this.starData = new StarData(getLogger());
         databaseManager = new MultidatabaseManager(starData);
         starData.setDatabaseManager(databaseManager);
-        //Databases
 
-        SqlProperties sqlProperties = new SqlProperties().setHost("localhost").setUsername("root").setPassword("niles3408").setPort(3306);
-        
-        databaseManager.setupDatabase(sqlProperties.clone().setDatabase("test"));
-        databaseManager.setupDatabase(sqlProperties.clone().setDatabase("test2"));
-        databaseManager.setupDatabase(sqlProperties.clone().setDatabase("test3"));
-        
-        databaseManager.registerObjectAsTable(TestOne.class, "test");
-        databaseManager.registerObjectAsTable(TestTwo.class, "test2");
-        databaseManager.registerObjectAsTable(TestThree.class, "test3");
-        
         databaseManager.setup();
-        
-        var testOneData = databaseManager.getAllData(TestOne.class, null, null);
-        var testThreeData = databaseManager.getAllData(TestThree.class, null, null);
-
-        for (TestOne tod : testOneData) {
-            getLogger().finest(tod.toString());
-        }
-
-        for (TestThree ttd : testThreeData) {
-            getLogger().finest(ttd.toString());
-        }
-    }
-
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!cmd.getName().equalsIgnoreCase("testdatabase")) {
-            return true;
-        }
-
-        Random random = new Random();
-        
-        for (int i = 0; i < random.nextInt(9) + 1; i++) {
-            TestOne testOne = new TestOne(new Code(8).toString());
-            for (int j = 0; j < random.nextInt(4) + 1; j++) {
-                testOne.generateTestTwo();
-            }
-            databaseManager.saveData(testOne);
-        }
-        
-        for (int i = 0; i < random.nextInt(5) + 1; i++) {
-            TestThree testThree = new TestThree(new Code(10).toString());
-            databaseManager.saveData(testThree);
-        }
-        
-        return true;
     }
 }
