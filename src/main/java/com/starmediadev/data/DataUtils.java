551fcca884cb3c7978fcbde7d2554558a294a8e1
Firestar311 <slaterandrew0398@gmail.com>
2022-01-11 16:48:27 -0600

    Added a general utility class

---
+++ src/main/java/com/starmediadev/data/DataUtils.java
@@ -0,0 +1,8 @@
+package com.starmediadev.data;
+
+/**
+ * This class is for shared methods and functionality between all features and abilities in this library to prevent repeat code
+ */
+public final class DataUtils {
+    private DataUtils() {}
+}
--- src/main/java/com/starmediadev/data/SQLDataType.java
+++ src/main/java/com/starmediadev/data/SQLDataType.java
@@ -1,5 +1,46 @@
 package com.starmediadev.data;
 
+/**
+ * The different data types for SQL based databases supported by this library
+ * This also contains the different Java Classes that they use and is used to verify serialized objects
+ */
 public enum SQLDataType {
-    VARCHAR, INT, DOUBLE, BIGINT 
+    VARCHAR(String.class), INT(Integer.class, int.class), DOUBLE(Double.class, double.class), BIGINT(Long.class, long.class);
+    
+    private final Class<?> mainJavaClass;
+    private final Class<?>[] additionalJavaClasses;
+    
+    SQLDataType(Class<?> mainJavaClass) {
+        this.mainJavaClass = mainJavaClass;
+        this.additionalJavaClasses = null;
+    }
+    
+    SQLDataType(Class<?> mainJavaClass, Class<?>... additionalJavaClasses) {
+        this.mainJavaClass = mainJavaClass;
+        this.additionalJavaClasses = additionalJavaClasses;
+    }
+    
+    public Class<?> getMainJavaClass() {
+        return mainJavaClass;
+    }
+    
+    public Class<?>[] getAdditionalJavaClasses() {
+        return additionalJavaClasses;
+    }
+    
+    public boolean isValidType(Class<?> clazz) {
+        if (clazz.getName().equals(mainJavaClass.getName())) {
+            return true;
+        } else {
+            for (Class<?> additionalJavaClass : additionalJavaClasses) {
+                return clazz.getName().equals(additionalJavaClass.getName());
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean isValidType(Object object) {
+        return isValidType(object.getClass());
+    }
 }
